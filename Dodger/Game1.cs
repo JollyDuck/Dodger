9123703b204735bcd9742fe28ccd5f5391f912c7
JollyDuck <jolly_duck@hotmail.co.uk>
2013-09-24 16:34:59 +0100

    Working three boxes

--- Dodger/Game1.cs
+++ Dodger/Game1.cs
@@ -18,19 +18,18 @@ namespace Dodger
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        int die1;
-        int die2;
-        int timeRolling;
-        int rollTime;
-        bool rolling;
-
+        TimeSpan Wave, resetTime;
+        byte buttonRepeatLimit;
+        int buttonReleaseTimer, wavesSurvived;
+        Vector2 thumb1, thumb2;
+        MinionManager minionManager;
         SpriteFont font;
-
         Random rand;
+        Texture2D redBox;
+        bool waitForReset;
 
         //Gamepad states to deturmine if buttons have been pushed.
         GamePadState currentGamePadState;
-        GamePadState previousGamePadState;
 
 
         public Game1()
@@ -49,13 +48,14 @@ namespace Dodger
         /// </summary>
         protected override void Initialize()
         {
-            die1 = 1;
-            die2 = 1;
-            timeRolling = 0;
-            rollTime = 1000;            
-            rolling = false;
+            minionManager = new MinionManager(15);
             rand = new Random();
+            waitForReset = false;
+            buttonRepeatLimit = 8;
+            wavesSurvived = 0;
             font = Content.Load<SpriteFont>("gameFont");
+            redBox = Content.Load<Texture2D>("redBox");
+            Wave = TimeSpan.Zero;
             // TODO: Add your initialization logic here
 
             base.Initialize();
@@ -89,29 +89,8 @@ namespace Dodger
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            currentGamePadState = GamePad.GetState(PlayerIndex.One);
-            if (!rolling)
-            {
-                if (currentGamePadState.Buttons.A == ButtonState.Pressed)
-                {
-                    rollDie();
-                    rolling = true;
-                }
-            }
-            else
-            {
-                timeRolling++;
-                if (timeRolling >= rollTime)
-                {
-                    rolling = false;
-                    timeRolling = 0;
-                }
-            }
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
-
-            // TODO: Add your update logic here
+            handleInput();
+            waveAttack(gameTime);
 
             base.Update(gameTime);
         }
@@ -124,19 +103,72 @@ namespace Dodger
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
-            spriteBatch.DrawString(font, "Text", new Vector2(50, 50), Color.Black);
-            spriteBatch.DrawString(font, die1.ToString(), new Vector2(50, 250), Color.Black);
-            spriteBatch.DrawString(font, die2.ToString(), new Vector2(50, 450), Color.Black);
+            drawBoxes(new Rectangle(64,64,64,64), new Rectangle(138,64,64,64), new Rectangle(212,64,64,64));
+            spriteBatch.DrawString(font, minionManager.minionsIn(0).ToString(), new Vector2(66, 66),Color.Black);
+            spriteBatch.DrawString(font, minionManager.minionsIn(1).ToString(), new Vector2(140, 66), Color.Black);
+            spriteBatch.DrawString(font, minionManager.minionsIn(2).ToString(), new Vector2(214, 66), Color.Black);
+            spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
 
-        void rollDie()
+        void handleInput()
         {
-            die1 = rand.Next(1, 7);
-            die2 = rand.Next(1, 7);
+            currentGamePadState = GamePad.GetState(PlayerIndex.One);
+            thumb1 = currentGamePadState.ThumbSticks.Left;
+            thumb2 = currentGamePadState.ThumbSticks.Right;
+
+            if (currentGamePadState.Buttons.LeftShoulder == ButtonState.Pressed)
+            {
+                if (buttonReleaseTimer > buttonRepeatLimit)
+                {
+                    minionManager.moveMinion(0);
+                    buttonReleaseTimer = 0;
+                }
+            }
+            else if (currentGamePadState.Buttons.RightShoulder == ButtonState.Pressed)
+            {
+                if (buttonReleaseTimer > buttonRepeatLimit)
+                {
+                    minionManager.moveMinion(2);
+                    buttonReleaseTimer = 0;
+                }
+            }
+
+            if (buttonReleaseTimer < buttonRepeatLimit + 1) buttonReleaseTimer++;
+
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+        }
+        void drawBoxes(Rectangle a, Rectangle b, Rectangle c)
+        {
+            spriteBatch.Draw(redBox, a, Color.White);
+            spriteBatch.Draw(redBox, b, Color.White);
+            spriteBatch.Draw(redBox, c, Color.White);
         }
 
+        void waveAttack(GameTime gameTime)
+        {
+            Wave += gameTime.ElapsedGameTime;
+
+            if (waitForReset)
+            {
+                resetTime += gameTime.ElapsedGameTime;
+                if (resetTime > TimeSpan.FromSeconds(1))
+                {
+
+                    minionManager.reset();
+                    waitForReset = false;
+                }
+            }
+            else if (Wave > TimeSpan.FromSeconds(15))
+            {
+                Wave = TimeSpan.Zero;
+                minionManager.killMinionsIn(rand.Next(0,3));
+                waitForReset = true;
+            }
+        }
     }
 }
---
+++ Dodger/MinionManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dodger
+{
+    class MinionManager
+    {
+        private Minion[] minArray;
+        private Minion currentMinion;
+        public int minionsAlive;
+
+        public MinionManager(int p)
+        {
+            minArray = new Minion[p];
+            minionsAlive = p;
+            for (int i = 0; i < p; i++)
+            {
+                minArray[i] = new Minion();
+            }
+        }
+
+        public void moveMinion(int location)
+        {
+            if (minInMid())
+            {
+                currentMinion.position = location;
+            }
+
+        }
+
+
+        private bool minInMid()
+        {
+
+            foreach (Minion element in minArray)
+            {
+                if (element.position == 1)
+                {
+                    currentMinion = element;
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+
+
+        public int minionsIn(int loc)
+        {
+            int count = 0;
+            foreach (Minion element in minArray)
+            {
+                if (element.position == loc) count++;
+            }
+            return count;
+        }
+
+
+
+        internal void killMinionsIn(int p)
+        {
+            foreach (Minion element in minArray)
+            {
+                if (element.position == p)
+                {
+                    element.position = -1;
+                    minionsAlive--;
+                }
+            }
+        }
+
+        internal void reset()
+        {
+            foreach (Minion element in minArray)
+            {
+                if (element.position != -1)
+                {
+                    element.position = 1;
+                }
+            }
+        }
+    }
+}
